522d41bd54353aa122d013912ab66380ee6ae51e
tsyxyz <tsyxyz@hotmail.com>
2020-09-12 21:45:20 +0800

    add 42_TrappingRainWater

---
+++ 42_TrappingRainWater.cpp
@@ -0,0 +1,100 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int pillarNum = height.size();
+        if (pillarNum < 2) {
+            return 0;
+        }
+        int highestHeight = getHighestHeight(height);
+        int totalArea = pillarNum * highestHeight;
+        int pillarArea = getPillarArea(height);
+        int leakArea = getLeakArea(height);
+        int result = totalArea - pillarArea - leakArea;
+
+        return result;
+    }
+
+private:
+    int getHighestHeight(const vector<int>& pillars) {
+        int highest = 0;
+        for (auto it : pillars) {
+            if (it > highest)
+                highest = it;
+        }
+        return highest;
+    }
+
+    int getPillarArea(const vector<int>& pillars) {
+        int area = 0;
+        for (auto it : pillars) {
+            area += it;
+        }
+        return area;
+    }
+
+    int getLeakArea(const vector<int>& pillars) {
+        return getLeftLeakArea(pillars) + getRightLeakArea(pillars);
+    }
+
+    int getLeftLeakArea(const vector<int>& pillars) {
+        vector<int> higherPillarsIdx;
+        int prevHeight = pillars[0];
+        higherPillarsIdx.push_back(0);
+        for (int i = 1; i < pillars.size(); ++i) {
+            if (pillars[i] > prevHeight) {
+                higherPillarsIdx.push_back(i);
+                prevHeight = pillars[i];
+            }
+        }
+        int area = 0;
+        int startHeight = pillars[higherPillarsIdx[0]];
+        for (int i = 0; i < higherPillarsIdx.size(); ++i) {
+            int dx = higherPillarsIdx[i];
+            int dy = pillars[higherPillarsIdx[i]] - startHeight;
+            area += (dx * dy);
+            startHeight = pillars[higherPillarsIdx[i]];
+        }
+        return area;
+    }
+
+    int getRightLeakArea(const vector<int>& pillars) {
+        vector<int> higherPillarsIdx;
+        int prevHeight = pillars[pillars.size() - 1];
+        higherPillarsIdx.push_back(pillars.size() - 1);
+        for (int i = static_cast<int>(pillars.size()) - 2; i >= 0; --i) {
+            if (pillars[i] > prevHeight) {
+                higherPillarsIdx.push_back(i);
+                prevHeight = pillars[i];
+            }
+        }
+        int area = 0;
+        int startHeight = pillars[higherPillarsIdx[0]];
+        for (int i = 0; i < higherPillarsIdx.size(); ++i) {
+            int dx = static_cast<int>(pillars.size()) - 1 - higherPillarsIdx[i];
+            int dy = pillars[higherPillarsIdx[i]] - startHeight;
+            area += (dx * dy);
+            startHeight = pillars[higherPillarsIdx[i]];
+        }
+        return area;
+    }
+};
+
+TEST(TestTrappingRainWater, case01) {
+    Solution solution;
+    vector<int> input = {4, 0, 6, 0, 5, 0, 7, 0, 5, 0, 6, 0, 4, 0, 3};
+    int result = solution.trap(input);
+    EXPECT_EQ(37, result);
+}
+
+TEST(TestTrappingRainWater, case02) {
+    Solution solution;
+    vector<int> input = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int result = solution.trap(input);
+    EXPECT_EQ(6, result);
+}
